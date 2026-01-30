8032214845927fc0c5ce4c56d0f53525089830b2
Aleksandar Karastoyanov <a.karastoyanov@utp.bg>
2026-01-31 00:13:34 +0200

    feat: Implemented User Roles class model:
    - added class model UserRoles with 1-to-1 relationship with User
    - new users get User role by default
    - added pytest tests
    
    Signed-off-by: Aleksandar Karastoyanov <a.karastoyanov@utp.bg>

--- backend/models/user_role.py
+++ backend/models/user_role.py
@@ -6,14 +6,14 @@ class UserRole(db.Model):
     Represents a user's role in the system.
 
     Attributes:
-        id (str): The unique identifier for the user's role. 1-to-1 relationship with User.
+        user_id (str): The unique identifier for the user's role. 1-to-1 relationship with User.
         role (str): The user's role (e.g., "User", "Admin", "Moderator").
         created_at (datetime): The timestamp when the role was created.
     """
 
     __tablename__ = "user_roles"
 
-    id = db.Column(
+    user_id = db.Column(
         db.String(36), db.ForeignKey("users.id", ondelete="CASCADE"), primary_key=True
     )
     role = db.Column(db.String(50), default="User", nullable=False)
