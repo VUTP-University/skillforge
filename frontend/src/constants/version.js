fc0dac73b541a511dc0e7fa2198d6fd158a757f9
Aleksandar Karastoyanov <a.karastoyanov@utp.bg>
2026-09-26 10:43:08 +0300

    chore: bump release date for v2.2.4

--- frontend/src/constants/version.js
+++ frontend/src/constants/version.js
@@ -3,5 +3,5 @@
  * wordmark (navbar + login/register). Bump both values together on
  * each release.
  */
-export const APP_VERSION = "2.2.3";
-export const RELEASE_DATE_LABEL = "Sep 22, 2026";
+export const APP_VERSION = "2.2.4";
+export const RELEASE_DATE_LABEL = "Sep 26, 2026";
